68c25ad83babad42816432f49b8e0068bdaaa1fe
rickyma <mr.rickyma@gmail.com>
2019-11-20 17:48:50 -0800

    Initial commit

---
+++ examscheduler.pl
@@ -0,0 +1,74 @@
+% Database of Facts
+course(cs103).
+course(cs110).
+course(cs121).
+course(cs210).
+course(cs213).
+course(cs221).
+course(cs260).
+course(cs261).
+course(cs302).
+course(cs304).
+course(cs310).
+course(cs311).
+course(cs313).
+course(cs314).
+course(cs317).
+course(cs319).
+course(cs320).
+course(cs322).
+course(cs340).
+course(cs344).
+course(cs349).
+course(cs404).
+course(cs406).
+course(cs415).
+course(cs418).
+course(cs420).
+course(cs421).
+course(cs422).
+course(cs424).
+course(cs444).
+course(cs445).
+course(cs449).
+prop(cs100, enrolled, 290).
+prop(cs103, enrolled, 459).
+prop(cs110, enrolled, 774).
+prop(cs121, enrolled, 455).
+prop(cs210, enrolled,).
+prop(cs213, enrolled,).
+prop(cs221, enrolled,).
+prop(cs260, enrolled,).
+prop(cs261, enrolled,).
+prop(cs302, enrolled,).
+prop(cs304, enrolled,).
+prop(cs310, enrolled,).
+prop(cs311, enrolled,).
+prop(cs312, enrolled, 124).
+prop(cs313, enrolled,).
+prop(cs314, enrolled,).
+prop(cs317, enrolled,).
+prop(cs319, enrolled,).
+prop(cs320, enrolled,).
+prop(cs322, enrolled,).
+prop(cs340, enrolled,).
+prop(cs344, enrolled,).
+prop(cs349, enrolled,).
+prop(cs404, enrolled,).
+prop(cs406, enrolled,).
+prop(cs415, enrolled,).
+prop(cs418, enrolled,).
+prop(cs420, enrolled,).
+prop(cs421, enrolled,).
+prop(cs422, enrolled,).
+prop(cs424, enrolled,).
+prop(cs444, enrolled,).
+prop(cs445, enrolled,).
+prop(cs449, enrolled,).
+
+
+prop(dmp310, capacity, 160).
+prop(dmp310, day, [2,3,4,5,6,9,10,11,12,13,16,17,18]).
+prop(dmp310, time, [0830,1200,1530]).
+
+% prop(day, valid, [2,3,4,5,6,9,10,11,12,13,16,17,18]).
